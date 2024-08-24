e07f0289a34242afc122156a2b8f2957b67de693
Jahari <jahari.crawford.young@gmail.com>
2024-08-24 10:50:55 -0400

    video header

--- ingenious/src/Components/Home Components/DiscoverImage.js
+++ ingenious/src/Components/Home Components/DiscoverImage.js
@@ -1,13 +1,14 @@
 import React from "react";
 import discoimg from "../../img/discover.png"
+import igMovie from '../../img/IngeniousMovie.mp4'
 
 
 export default function DiscoverImage (){
     return (
         <div className="discover-container">
-            <img src={discoimg} alt="" className="discoimg" />
+            <video loop autoPlay src={igMovie} alt="" className="discoimg" />
             <div className="discover-overlay">
-                <span className="discover-text--head">Ingenious Dealer</span>
+                <span className="discover-text--head">Ingenious Dealers</span>
                 <h1 className="discover-text--sub">Discover A Fashion Hub In Your City</h1>
                 <div className="discover-btn-group">
                     <button>Explore Now</button>
--- ingenious/src/pages/Home.js
+++ ingenious/src/pages/Home.js
@@ -24,7 +24,6 @@ const Home = () => {
             </div>
             <ServiceSearch />
             <Amenities />
-            <HomeCard />
             <PhotoGrid />
             <Footer />
         </motion.div>
