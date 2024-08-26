1af4cc2baf43af7e6a6d62cd9935e4890c757375
Jahari <jahari.crawford.young@gmail.com>
2024-08-26 13:37:51 -0400

    password page bg

--- ingenious/src/Components/Password Component/PasswordForm.jsx
+++ ingenious/src/Components/Password Component/PasswordForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion'
+import IngeniousBackground from '../../img/IngeniousPasswordPage.mp4'
 
 
 const PasswordForm = ({ setPassword }) => {
@@ -26,32 +27,35 @@ const PasswordForm = ({ setPassword }) => {
         animate={{opacity: 1}}
         exit={{opacity: 0}}
         transition={{duration: 0.5}}
+
     >
-    <div className='password-page'>
-      <form onSubmit={handleSubmit}>
-        <div className="password-input-container">
+      <video className='absolute top-0 left-0 w-full h-full object-cover z-0' src={IngeniousBackground} autoPlay loop muted playsInline />
+
+      <div className="relative z-10 flex items-center justify-center flex-col h-screen">
+        <form onSubmit={handleSubmit}>
+          <div className="password-input-container">
+            <input
+              className='p-input'
+              type={showPassword ? "text" : "password"}
+              placeholder='Enter Password'
+              value={passwordInput}
+              onChange={(e) => setPasswordInput(e.target.value)}
+            />
+            <button type='submit' className="arrow-button">
+              <i className="fa-solid fa-arrow-right"></i>
+            </button>
+          </div>
+        </form>
+        <div className='pw-check'>
           <input
-            className='p-input'
-            type={showPassword ? "text" : "password"}
-            placeholder='Enter Password'
-            value={passwordInput}
-            onChange={(e) => setPasswordInput(e.target.value)}
+            type="checkbox"
+            onChange={togglePasswordVisibility}
+            checked={showPassword}
           />
-          <button type='submit' className="arrow-button">
-            <i className="fa-solid fa-arrow-right"></i>
-          </button>
+          <span>Show Password</span>
         </div>
-      </form>
-      <div className='pw-check'>
-        <input
-          type="checkbox"
-          onChange={togglePasswordVisibility}
-          checked={showPassword}
-        />
-        <span>Show Password</span>
+        {errorMessage && <div style={{color: 'red'}}>{errorMessage}</div>}
       </div>
-      {errorMessage && <div style={{color: 'red'}}>{errorMessage}</div>}
-    </div>
     </motion.div>
   );
 }
