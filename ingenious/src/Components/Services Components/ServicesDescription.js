44f90e9d91a27a6599e231872c8137e3b2add04e
Jahari <jahari.crawford.young@gmail.com>
2024-09-09 00:06:08 -0400

    9/8 pt2

--- ingenious/src/Components/Services Components/ServicesDescription.js
+++ ingenious/src/Components/Services Components/ServicesDescription.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function ServicesDescription(){
     return (
         <div className="flex items-center justify-center flex-col">
-        <h1 className="font-bold text-lg">Redefine what it means to be a dealer in the community</h1>
-        <p className="font-bold text-lg">Discover our clothing services for tailored creations, collaborating with photographers and models to craft compelling fashion campaigns.Ingenious presents an exclusive auction platform for selling new collections and merchandise</p>
+        <h1 className="font-bold text-lg text-center">Redefine what it means to be a dealer in the community</h1>
+        <p className="font-bold text-lg text-center">Discover our clothing services for tailored creations, collaborating with photographers and models to craft compelling fashion campaigns.Ingenious presents an exclusive auction platform for selling new collections and merchandise</p>
         
         </div>
     )
