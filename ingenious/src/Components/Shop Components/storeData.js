bd91b44ec5e919f84079f8070eca47514e4594fd
Jahari <jahari.crawford.young@gmail.com>
2024-09-18 09:43:16 -0400

    9.18fix

--- ingenious/src/Components/Shop Components/storeData.js
+++ ingenious/src/Components/Shop Components/storeData.js
@@ -37,7 +37,7 @@ const storeData = [
     },
     {
         id: 6,
-        storeName: "Club Monac",
+        storeName: "Club Monaco",
         storeLocation: "899 Central Ave Albany, NY 12206",
         storeImage: "https://images.unsplash.com/photo-1519822472072-ec86d5ab6f5c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         storeDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
